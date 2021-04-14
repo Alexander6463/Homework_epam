fc787208d24e9a69a506de4182d62f59e5400405
Alexander6463 <drobnov1994@mail.ru>
2021-04-14 12:30:53 +0300

    Pytest and flake8 added at the pre-commit config

--- homework1/tests/test_02.py
+++ homework1/tests/test_02.py
@@ -4,7 +4,8 @@ from homework1.hw.task02 import check_fibonacci
 def test_positive_case():
     """Testing that sequence is fibonacci sequence"""
     assert check_fibonacci(
-        [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597]
+        [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55,
+         89, 144, 233, 377, 610, 987, 1597]
     )
 
 
--- homework1/tests/test_03.py
+++ homework1/tests/test_03.py
@@ -1,10 +1,20 @@
+import os
+
 from homework1.hw.task03 import find_maximum_and_minimum
+
+print(os.getcwd())
 
 
 def test_min_max():
     """Testing list of numbers [-5, -3, 0, 5, 10]"""
-    assert find_maximum_and_minimum("files_for_test/example1.txt") == (-5, 10)
+    assert find_maximum_and_minimum("homework1/"
+                                    "tests/"
+                                    "files_for_test/"
+                                    "example1.txt") == (-5, 10)
 
 
 def test_equal_numbers():
-    assert find_maximum_and_minimum("files_for_test/example2.txt") == (5, 5)
+    assert find_maximum_and_minimum("homework1/"
+                                    "tests/"
+                                    "files_for_test/"
+                                    "example2.txt") == (5, 5)

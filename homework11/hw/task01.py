3c844b96fa29ee77181eaf8cc04f767320083dc8
Alexander6463 <drobnov1994@mail.ru>
2021-06-15 11:15:21 +0300

    Homework #11 (parallelization)
    - rename vars, methods
    - delete prints

--- homework11/hw/task01.py
+++ homework11/hw/task01.py
@@ -10,11 +10,11 @@ from bs4 import BeautifulSoup
 class CompanyRepository:
     def __init__(self):
         self.companies = list()
-        self.dollar = float()
+        self.dollar_value = float()
         self.pages = list()
-        self.firms = list()
+        self.companies_info_link_cost_growth = list()
         self.new_urls = list()
-        self.firms2 = list()
+        self.companies_info_name_pe_52wl_52wh = list()
 
     @staticmethod
     async def fetch_response(url):
@@ -32,82 +32,83 @@ class CompanyRepository:
         return pages
 
     @staticmethod
-    def get_url_for_first_part():
+    def get_urls_for_parse_link_cost_growth():
         base_url = 'https://markets.businessinsider.com/' \
                    'index/components/s&p_500?p='
         urls = [base_url + str(i) for i in range(1, 11)]
         return urls
 
     @staticmethod
-    def get_url_for_second_part(lst):
+    def get_url_for_parse_name_pe_52wl_52wh(companies_info):
         urls = list()
-        for firm in chain(*lst):
-            urls.append('https://markets.businessinsider.com' + firm['link'])
+        for company_info in chain(*companies_info):
+            urls.append('https://markets.businessinsider.com' +
+                        company_info['link'])
         return urls
 
     @staticmethod
-    def get_first_part_info_from_html(html):
-        result = list()
+    def get_link_cost_growth_from_html(html):
+        company_info = list()
         soup = BeautifulSoup(html, 'html.parser')
         for tr in soup.find_all('tr'):
-            current_dict = {"link": None, "cost": 0, 'growth': 0}
+            temp_dict = {"link": None, "cost": 0, 'growth': 0}
             info = tr.get_text().replace('\n\t', '').split()
             growth = info[-1]
             for td in tr:
                 if 'title' in str(td):
                     link = td.find('a')['href']
-                    current_dict.update({'link': link})
+                    temp_dict.update({'link': link})
                 elif "table__td" in str(td):
                     cost = float(td.get_text().split()[0].replace(',', ''))
-                    current_dict.update({'cost': cost,
-                                         'growth':
-                                             float(growth.replace('%', ''))})
-                    result.append(current_dict)
+                    temp_dict.update({'cost': cost,
+                                      'growth':
+                                          float(growth.replace('%', ''))})
+                    company_info.append(temp_dict)
                     break
-        return result
+        return company_info
 
     @staticmethod
-    def get_second_part_info_from_html(html):
-        current_dict = {'name': None, 'P/E': 0, '52WL': 0, '52WH': 0}
+    def get_name_pe_52wl_52wh_from_html(html):
+        company_info = {'name': None, 'P/E': 0, '52WL': 0, '52WH': 0}
         soup = BeautifulSoup(html, 'html.parser')
         for h1 in soup.find_all('h1'):
             for span in h1.find('span'):  # get name company
-                current_dict.update({'name': span.strip()})
+                company_info.update({'name': span.strip()})
         for div_tag in soup.find_all('div',
                                      {'class': "snapshot__data-item"}):
             text = div_tag.get_text()
             if 'P/E' in text:
-                current_dict.update({
+                company_info.update({
                     "P/E": float(text.split()[0].replace(',', ''))})
             if '52 Week Low' in text:
-                current_dict.update({
+                company_info.update({
                     '52WL': float(text.split()[0].replace(',', ''))})
             if '52 Week High' in text:
-                current_dict.update({
+                company_info.update({
                     '52WH': float(text.split()[0].replace(',', ''))})
-        return current_dict
+        return company_info
 
-    async def get_current_value_dollar(self):
-        xml = await self.fetch_response(
+    async def get_value_dollar(self):
+        html = await self.fetch_response(
             'http://www.cbr.ru/scripts/XML_daily.asp?date_req=07/06/2021')
-        soup = BeautifulSoup(xml, 'html.parser')
+        soup = BeautifulSoup(html, 'html.parser')
         for valute in soup.find_all('valute'):
             if 'USD' in valute.get_text():
                 return float(valute.find('value').get_text().
                              replace(',', '.'))
 
     @staticmethod
-    def update_info_firm1_and_firm2(firms1, firms2):
-        firm_result = list()
-        for firm in zip(chain(*firms1), firms2):
-            new_dict = dict()
-            link = firm[0]['link']
-            firm[0]['link'] = link.replace('/stocks/', '').\
+    def unite_information_about_companies(company_info_1, company_info_2):
+        companies_info = list()
+        for company in zip(chain(*company_info_1), company_info_2):
+            unite_company_info = dict()
+            link = company[0]['link']
+            company[0]['link'] = link.replace('/stocks/', '').\
                 replace('-stock', '')
-            new_dict.update(firm[1])
-            new_dict.update(firm[0])
-            firm_result.append(new_dict)
-        return firm_result
+            unite_company_info.update(company[1])
+            unite_company_info.update(company[0])
+            companies_info.append(unite_company_info)
+        return companies_info
 
     @staticmethod
     def parse_info_multiprocess(func, html):
@@ -117,19 +118,22 @@ class CompanyRepository:
 
     async def get_info_companies(self):
         self.pages = await self.get_result_tasks(
-            self.get_url_for_first_part(),
+            self.get_urls_for_parse_link_cost_growth(),
             self.fetch_response)
-        self.firms = self.parse_info_multiprocess(
-            self.get_first_part_info_from_html,
+        self.companies_info_link_cost_growth = self.parse_info_multiprocess(
+            self.get_link_cost_growth_from_html,
             self.pages)
-        self.new_urls = self.get_url_for_second_part(self.firms)
+        self.new_urls = self.get_url_for_parse_name_pe_52wl_52wh(
+            self.companies_info_link_cost_growth)
         self.pages = await self.get_result_tasks(
-            self.get_url_for_second_part(self.firms),
-            self.fetch_response)
-        self.firms2 = self.parse_info_multiprocess(
-            self.get_second_part_info_from_html,
+            self.get_url_for_parse_name_pe_52wl_52wh(
+                self.companies_info_link_cost_growth), self.fetch_response)
+        self.companies_info_name_pe_52wl_52wh = self.parse_info_multiprocess(
+            self.get_name_pe_52wl_52wh_from_html,
             self.pages)
-        return self.update_info_firm1_and_firm2(self.firms, self.firms2)
+        return self.unite_information_about_companies(
+            self.companies_info_link_cost_growth,
+            self.companies_info_name_pe_52wl_52wh)
 
 
 class Company:
@@ -144,65 +148,70 @@ class Company:
 
 
 class MakeReport:
-    def __init__(self, firms, dollar):
-        self.companies = firms
+    def __init__(self, companies_info, dollar):
+        self.companies_info = companies_info
         self.cost_dollar = dollar
 
     def get_cost_report(self):
         res_json = list()
-        for element in sorted(self.companies,
-                              key=lambda i: i['cost'], reverse=True)[0:10]:
-            company = Company(element)
+        for company_info in sorted(self.companies_info,
+                                   key=lambda i: i['cost'],
+                                   reverse=True)[0:10]:
+            company = Company(company_info)
             res_json.append(
                 {'code': company.link.upper(),
                  'name': company.name,
                  'price': round(company.cost * self.cost_dollar, 2)})
-        return json.dumps(res_json)
+        with open('cost_report.json', 'w') as outfile:
+            json.dump(res_json, outfile)
 
     def get_p_e_report(self):
         res_json = list()
-        for element in sorted(self.companies,
-                              key=lambda i: i['P/E'])[0:10]:
-            company = Company(element)
+        for company_info in sorted(self.companies_info,
+                                   key=lambda i: i['P/E'])[0:10]:
+            company = Company(company_info)
             res_json.append({'code': company.link.upper(),
                              'name': company.name,
                              'P/E': company.pe_index})
-        return json.dumps(res_json)
+        with open('p_e_report.json', 'w') as outfile:
+            json.dump(res_json, outfile)
 
     def get_growth_report(self):
         res_json = list()
-        for element in sorted(self.companies,
-                              key=lambda i: i['growth'], reverse=True)[0:10]:
-            company = Company(element)
+        for company_info in sorted(self.companies_info,
+                                   key=lambda i: i['growth'],
+                                   reverse=True)[0:10]:
+            company = Company(company_info)
             res_json.append({'code': company.link.upper(),
                              'name': company.name,
                              'growth': company.growth})
-        return json.dumps(res_json)
+        with open('growth_report.json', 'w') as outfile:
+            json.dump(res_json, outfile)
 
     def get_potential_profit_report(self):
         res_json = list()
-        for element in self.companies:
-            company = Company(element)
+        for company_info in self.companies_info:
+            company = Company(company_info)
             res_json.append({'code': company.link.upper(),
                              'name': company.name,
                              'potential profit':
                                  round((company.week_high_52 -
                                         company.week_low_52) *
                                        self.cost_dollar, 2)})
-        return json.dumps(sorted(res_json,
-                                 key=lambda i: i['potential profit'],
-                                 reverse=True)[0:10])
+        with open('potential_profit_report.json', 'w') as outfile:
+            json.dump(sorted(res_json,
+                             key=lambda i: i['potential profit'],
+                             reverse=True)[0:10], outfile)
 
 
 async def main():
-    firms = await CompanyRepository().get_info_companies()
-    cost_dollar = await CompanyRepository().get_current_value_dollar()
-    report = MakeReport(firms, cost_dollar)
-    print(firms)
-    print(report.get_cost_report())
-    print(report.get_p_e_report())
-    print(report.get_growth_report())
-    print(report.get_potential_profit_report())
+    companies_information = await CompanyRepository().get_info_companies()
+    cost_dollar = await CompanyRepository().get_value_dollar()
+    report = MakeReport(companies_information, cost_dollar)
+    report.get_cost_report()
+    report.get_p_e_report()
+    report.get_growth_report()
+    report.get_potential_profit_report()
 
 
 if __name__ == '__main__':
--- homework11/test/test_01.py
+++ homework11/test/test_01.py
@@ -1,4 +1,5 @@
 import json
+import os
 from unittest.mock import AsyncMock
 
 import pytest
@@ -19,39 +20,49 @@ async def input_data():
 @pytest.fixture()
 async def get_cost_of_dollar():
     company = CompanyRepository()
-    company.get_current_value_dollar = AsyncMock(return_value=71.85)
-    return await company.get_current_value_dollar()
+    company.get_value_dollar = AsyncMock(return_value=71.85)
+    return await company.get_value_dollar()
 
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio()
 async def test_pe_max_report(input_data, get_cost_of_dollar):
-    assert await json.loads(
-        MakeReport(input_data, get_cost_of_dollar).
-        get_p_e_report())[0] == {'code': 'VNO',
-                                 'name': 'Vornado Realty Trust',
-                                 'P/E': -893.45}
+    MakeReport(await input_data, await get_cost_of_dollar).get_p_e_report()
+    with open('p_e_report.json', 'r') as input_json:
+        assert json.loads(input_json.read())[0] == \
+               {'code': 'VNO',
+                'name': 'Vornado Realty Trust',
+                'P/E': -893.45}
+    os.remove('p_e_report.json')
 
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio()
 async def test_cost_max_report(input_data, get_cost_of_dollar):
-    assert await json.loads(MakeReport(input_data, get_cost_of_dollar).
-                            get_cost_report())[0] == {'code': 'AMZN',
-                                                      'name': 'Amazon',
-                                                      'price': 238932.86}
+    MakeReport(await input_data, await get_cost_of_dollar).get_cost_report()
+    with open('cost_report.json', 'r') as input_json:
+        assert json.loads(input_json.read())[0] == \
+               {'code': 'AMZN',
+                'name': 'Amazon',
+                'price': 238932.86}
+    os.remove('cost_report.json')
 
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio()
 async def test_growth_max_report(input_data, get_cost_of_dollar):
-    assert await json.loads(MakeReport(input_data, get_cost_of_dollar).
-                            get_growth_report())[0] == {'code': 'LB',
-                                                        'name': 'L Brands Inc',
-                                                        'growth': 347.65}
+    MakeReport(await input_data, await get_cost_of_dollar).get_growth_report()
+    with open('growth_report.json', 'r') as input_json:
+        assert json.loads(input_json.read())[0] == {'code': 'LB',
+                                                    'name': 'L Brands Inc',
+                                                    'growth': 347.65}
+    os.remove('growth_report.json')
 
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio()
 async def test_potential_profit_max_report(input_data, get_cost_of_dollar):
-    assert await json.loads(MakeReport(input_data, get_cost_of_dollar).
-                            get_potential_profit_report())[0] \
-           == {'code': 'GOOG',
-               'name': 'Alphabet C (ex Google)',
-               'potential profit': 84235.5}
+    MakeReport(await input_data, await get_cost_of_dollar).\
+        get_potential_profit_report()
+    with open('potential_profit_report.json', 'r') as input_json:
+        assert json.loads(input_json.read())[0] == \
+               {'code': 'GOOG',
+                'name': 'Alphabet C (ex Google)',
+                'potential profit': 84235.5}
+    os.remove('potential_profit_report.json')
